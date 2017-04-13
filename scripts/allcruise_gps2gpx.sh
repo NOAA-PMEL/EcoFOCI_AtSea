1eb0a254a344ca37bee5592bc2c073127a9b153f
shaunwbell <shaun.w.bell@gmail.com>
2017-04-13 10:27:13 -0700

    routine to create gpx files from SCS gps files

---
+++ scripts/allcruise_gps2gpx.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Purpose:
+#       Script to run SCS_shptrack2gpx.py for each file in a directory
+#       and output as independant files per day
+
+cruiseid='DY1608L3'
+cruiseyear='2016'
+
+data_dir="/Users/bell/ecoraid/${cruiseyear}/AlongTrack/${cruiseid}/SCSData/GPSGAR17N/*GPGGA*"
+prog_dir="/Users/bell/Programs/Python/EcoFOCI_AtSea/"
+out_dir="/Users/bell/scratch/"
+
+for files in $data_dir
+do
+    names=(${files//\// })
+    outfile=${names[${#names[@]} - 1]}
+    echo "processing file: $files"
+    python ${prog_dir}SCS_shptrack2gpx.py -i ${files}  >> ${out_dir}${outfile}.gpx
+done
