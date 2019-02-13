d94a5dfe2a1b353c9fcd197f8344a2e55308caf4
shaunbell <shaun.bell@noaa.gov>
2019-02-12 16:28:42 -0800

    change source of config files

--- PostCruiseMetaDBadd.py
+++ PostCruiseMetaDBadd.py
@@ -96,7 +96,7 @@ def read_data(db, cursor, table, cruiseID, legNO=''):
 def AddMeta_fromDB(user_in, user_out, cruiseID, server='pavlof'):
             
     table='cruisecastlogs'
-    db_config = ConfigParserLocal.get_config('../EcoFOCI_Config/AtSeaPrograms/config_files/db_config_cruises.yaml','yaml')
+    db_config = ConfigParserLocal.get_config('../EcoFOCI_Config/EcoFOCI_AtSea/db_config_cruises.yaml','yaml')
     if server == 'pavlof':
         host='pavlof'
     else:
